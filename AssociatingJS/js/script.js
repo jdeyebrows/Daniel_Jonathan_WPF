ad8e465d3a050aa78cc48cb8d033fb8493f65854
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 08:49:38 -0600

    Commented out scripts
    Commented out previous scripts

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -13,45 +13,60 @@ console.log(yearBorn); */
 
 // declare before defining
 
-    /* var a = 2;
+  /*  var a = 2;
     var b;
     b = a + 3;
-    console.log(a + 3); */
+    console.log(a + 3);
 
-    /* var yearBorn = 1979;
+    var yearBorn = 1979;
     var age= 2014 - yearBorn;
-    console.log(age); */
+    console.log(age);
 
 // area of a triangle is half the width times the height
-    /* var width = 4;
+    var width = 4;
     var height = 5;
     var area = width * height/2;
-    console.log(area); */
+    console.log(area);
 
-    /* var quiz1 = 87;
+    var quiz1 = 87;
     var quiz2 = 100;
     var quiz3 = 60;
-    var quiz4 = 80; */
+    var quiz4 = 80;
 
 // The average is the sum of the numbers divided by the number of items.
-    /* var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-    // console.log(average); */
+    var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+    console.log(average);
 
-    /* var firstName = "Jonathan";
+    var firstName = "Jonathan";
     var lastName = "Daniel";
     var fullName = firstName + " " + lastName;
 
-console.log(fullName); */
+    console.log(fullName);
 
     var a = "6";
     var b = "7";
     var result = a + b;
     console.log(result); /* concatenating string */
 
-    var a = 6;
+    /* var a = 6;
     var b = 7;
     var result = a + b;
     console.log(result); /* adding the numbers */
+
+    /* var stringVar = "6";
+    var result = 7 + Number(stringVar);
+    console.log(result);
+
+    var areaCode = 870;
+    var firstPart = 678;
+    var secPart = 3379;
+
+    var phoneNo = "(" + String(areaCode) + ")" + String(firstPart)+ "-" + String(secPart);
+    console.log(phoneNo); */
+
+var userInput = prompt("Enter your year of birth:")
+console.log(userInput);
+
 
 
 
