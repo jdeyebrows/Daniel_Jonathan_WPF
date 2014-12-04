52ef4ad7318a8a7d556895342ad1f49066f1141f
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-03 21:30:39 -0600

    Added First Variables
    Added First Variables Declaration and Definition of age.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,22 @@
 alert("test");
+
+var yearBorn = 1979
+
+/* console.log("Hello World");
+console.log(yearBorn); */
+
+alert("yearBorn");
+
+var age; // decraration
+    age = 35; // definition
+// storage container = value
+
+// declare before defining
+
+    var a = 2;
+    var b;
+    b = a + 3;
+    console.log(a + 3);
+
+
+
