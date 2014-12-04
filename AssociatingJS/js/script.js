06ef34b9b30bf1f5151071260f9e0eba90054c1c
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 07:00:21 -0600

    Commented out scripts
    Commented out scripts

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,45 +1,50 @@
-alert("test");
+/* alert("test");
 
-var yearBorn = 1979
+var yearBorn = 1979 */
 
 /* console.log("Hello World");
 console.log(yearBorn); */
 
-alert("yearBorn");
+/* alert("yearBorn"); */
 
-    var age; // decraration
-    age = 35; // definition
+    /* var age; // decraration
+    age = 35; // definition */
 // storage container = value
 
 // declare before defining
 
-    var a = 2;
+    /* var a = 2;
     var b;
     b = a + 3;
-    console.log(a + 3);
+    console.log(a + 3); */
 
-    var yearBorn = 1979;
+    /* var yearBorn = 1979;
     var age= 2014 - yearBorn;
-    console.log(age);
+    console.log(age); */
 
 // area of a triangle is half the width times the height
-    var width = 4;
+    /* var width = 4;
     var height = 5;
     var area = width * height/2;
-    console.log(area);
+    console.log(area); */
 
-    var quiz1 = 87;
+    /* var quiz1 = 87;
     var quiz2 = 100;
     var quiz3 = 60;
-    var quiz4 = 80;
+    var quiz4 = 80; */
 
 // The average is the sum of the numbers divided by the number of items.
-    var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-    // console.log(average);
-    var firstName = "Jonathan";
-    var lastName = "Daniel";
-    var fullName = firstName + lastName;
+    /* var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+    // console.log(average); */
 
-console.log(fullName);
+    /* var firstName = "Jonathan";
+    var lastName = "Daniel";
+    var fullName = firstName + " " + lastName;
+
+console.log(fullName); */
+
+
+
+
 
 
