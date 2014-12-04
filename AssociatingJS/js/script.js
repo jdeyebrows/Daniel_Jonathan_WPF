d5060ad1afe114b984a472778e6aae4f1bb1fd93
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-03 21:48:09 -0600

    Added first arithmetic operators
    Added first arithmetic operators to calculate area.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -7,7 +7,7 @@ console.log(yearBorn); */
 
 alert("yearBorn");
 
-var age; // decraration
+    var age; // decraration
     age = 35; // definition
 // storage container = value
 
@@ -17,6 +17,16 @@ var age; // decraration
     var b;
     b = a + 3;
     console.log(a + 3);
+
+    var yearBorn = 1979;
+    var age= 2014 - yearBorn;
+    console.log(age);
+
+// area of a triangle is half the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
 
 
 
