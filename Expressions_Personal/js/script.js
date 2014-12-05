4bbf2c344b5742415b95c93df335f77f26a6338e
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 20:30:32 -0600

    Edited prompt
    Edited information on height prompt line.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,15 +2,9 @@
  * Jonathan Daniel Expressions_Personal 12/4/14.
  */
 
-/* calculator for the volume of space needed to fit MIDI controller to workspace. */
+/* This is calculator for the volume of space needed to fit MIDI controller to workspace. */
 
-/* V = L * W * H */
-var a = 12.75; //Length
-var b = 11.5; //Width
-var c = 2; //Height
 
-var d = a * b * c; //Volume
-console.log(d);
 
 var width = prompt("We are calculating the needed area for an inset MIDI controller space. \nPlease enter the width.")
 
@@ -20,8 +14,10 @@ var height = prompt("Please enter the height.")
 
 
 
-var length = prompt("Enter Length")
+var length = prompt("Enter the Length")
 
 
 
-var volume = width * height * length;
+var volume = width * height * length; //calculates with that info
+
+console.log(volume); //prints it to console
