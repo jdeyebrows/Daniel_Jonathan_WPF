f9d4fd4b1572f603a1de48ea5d16bec3b429979e
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 18:14:13 -0600

    Added givens length and height
    Added givens length and height to Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,4 +2,9 @@
  * Jonathan Daniel Expressions_Personal 12/4/14.
  */
 
-var
+/* calculator for the volume of space needed to inset Native Instruments Maschine to workspace. */
+
+/* V = L * W * H */
+var length = 12.75;
+var height = 2;
+
