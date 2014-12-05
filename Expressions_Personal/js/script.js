e460fd149b2f8704988f128da5f78b7b7d8f1f18
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 22:30:22 -0600

    Updated prompt lines
    Updated prompt lines to request data be entered in inches without units.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,15 +6,15 @@
 
 
 
-var width = prompt("We are calculating the needed area for an inset MIDI controller space. \nPlease enter the width in inches.")
+var width = prompt("We are calculating the needed area for an inset MIDI controller space. \nPlease enter the width in inches without unit.")
 
 
 
-var height = prompt("Please enter the height in inches.")
+var height = prompt("Please enter the height in inches without unit.")
 
 
 
-var length = prompt("Enter the Length in inches")
+var length = prompt("Enter the Length in inches without unit")
 
 
 
@@ -22,4 +22,4 @@ var volume = width * height * length; //calculates with that info
 
 //console.log(volume); //prints it to console
 
-alert("The required volume for your MIDI controller inset space is" + volume + "inches");
+alert("The required volume for your MIDI controller inset space is" + " " + volume + " " + "inches");
