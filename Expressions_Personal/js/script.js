855dff6ed92cd97afbd6b64c058efd77ab6a9d66
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-04 18:27:38 -0600

    Added Givens
    Added Givens for length width and height.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,9 +2,10 @@
  * Jonathan Daniel Expressions_Personal 12/4/14.
  */
 
-/* calculator for the volume of space needed to inset Native Instruments Maschine to workspace. */
+/* calculator for the volume of space needed to fit MIDI controller to workspace. */
 
 /* V = L * W * H */
-var length = 12.75;
-var height = 2;
+var a = 12.75; //Length
+var b = 11.5; //Width
+var c = 2; //Height
 
