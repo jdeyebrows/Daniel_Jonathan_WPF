568cf9afd8e5709eb8a533e7e6f929b66c62cc18
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-06 17:35:59 -0600

    Started Wacky
    Started adding to js file for Expressions_Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,26 @@
 /**
- * Created by jonathandaniel on 12/4/14.
+ * Jonathan Daniel Expressions_Wacky 12/6/14.
  */
+
+/* This is calculator for the amount of hours you will have to work
+to make your desired amount of money considering your wage and fee. */
+
+
+
+var wage = prompt("We are calculating the number of web developers needed to complete a website in a given number of days based on a given set of variables . \nPlease enter the number of developers of your first given variables.")
+
+
+
+var numberhr = prompt("Enter the number of days the first number of developers will need to complete the website.")
+
+
+
+var mycut = prompt("Enter the new number of days to determine how many developers you will need \nto complete the website in that number of days")
+
+
+
+var developers2 = wage * numberhr - mycut; //calculates with that info
+
+//console.log(volume); //prints it to console
+
+alert("You will need" + " " + developers2 + " " + "developers to finish the website in" + days2 + "days.");
