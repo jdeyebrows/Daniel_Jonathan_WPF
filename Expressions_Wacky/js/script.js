4d9d8b9f19dc0c9824f8afe4d75415ee3c2005be
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-06 18:21:32 -0600

    corrected operation
    changed an operation from multiplication to division.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,7 +7,7 @@ to make your desired amount of money considering your wage and fee. */
 
 
 
-var wage = prompt("This is calculator for the amount of hours you will have to work to make your desired amount of money \nconsidering how much you hake and hour and fee. \nPlease enter The total amount You want to be paid for this job.")
+var wage = prompt("This is calculator for the amount of hours you will have to work to make your desired amount of money \nconsidering how much you hake and hour and fee. \nPlease enter The total dollar amount you want to be paid for this job.")
 
 
 
@@ -19,8 +19,8 @@ var mycut = prompt("Enter the dollar amount we have agreed would be my cut.")
 
 
 
-var numHrs = wage * dollarhr - mycut; //calculates with that info
+var numHrs = wage / dollarhr - mycut; //calculates with that info
 
 //console.log(volume); //prints it to console
 
-alert("You will need to work" + " " + numHrs + " " + "making" + " " + dollarhr + " " + "an hour to make the" + " " + wage + " " + "dollars.");
+alert("You will need to work" + " " + numHrs + " " + "making" + " " + dollarhr + " " + " dollars an hour to make the" + " " + wage + " " + "dollars.");
