6b92759d4b0a8882a87069815dc0705f28c892d1
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-06 22:34:07 -0600

    Comment Lines
    Added comment lines to code.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,19 +7,20 @@
 
 
 var developers1 = prompt("We are calculating the number of web developers needed to complete a website in a given number of days based on a given set of variables . \nPlease enter the number of developers of your first given variables.")
-
+//This line of code collects a variable that represents a given number on web developers.
 
 
 var days1 = prompt("Enter the number of days the first number of developers will need to complete the website.")
-
+//This line of code collects a variable that represents a given number of days the given number of web developers to finish the website.
 
 
 var days2 = prompt("Enter the new number of days to determine how many developers you will need \nto complete the website in that number of days")
+//This line of code collects the new number of days to be used to determine how many developers will be needed.
 
 
+var developers2 = developers1 * days1 / days2;
+//This line of code uses multiplication and division to determint the number of web developers will be needed to complete the website in the new number of days.
 
-var developers2 = developers1 * days1 / days2; //calculates with that info
-
-//console.log(volume); //prints it to console
+console.log(developers2); //prints it to console
 
 alert("You will need" + " " + developers2 + " " + "developers to finish the website in" + days2 + "days.");
