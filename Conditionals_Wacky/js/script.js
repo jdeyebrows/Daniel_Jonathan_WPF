df5e94148c4c94d0f4a42fab92b89a301c8f56c9
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-11 16:31:19 -0600

    linked js file.
    Linked script.js to index.html for Conditionals Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -11,17 +11,18 @@
   * compare it to the polygon limit of the machine you are using, and determine
   * whether or not your primary machine has been returned.*/
 
-var machLimit = prompt("You have been commissioned to design a 3D scene for a short" +
-" animated film. You are very capable of this job, but your freeloader friend has borrowed" +
-" your most powerful machine and kept it much longer than you agreed they could. What is" +
-" the highest number of polygons you can have in a character for it to work effectively in the scene.");
+var machLimit = prompt("You have been commissioned to design a 3D scene for a short film. What is the highest number of polygons you can have in a character model for it to work effectively in the scene.");
 //This portion of code collects the information about the polygon limit of the secondary machine.
 
 var modelPoly = prompt("Enter the number of polygons in your character model.");
 //This portion of code collects the value for the number of polygons in the model the user wishes to use.
 
-var machRtrn = prompt("Has your freeloading friend showed up with your machine yet? Yes or No");
+var machRtn = prompt("Has your freeloading friend showed up with your machine yet? Yes or No");
 
-if(modelPoly <= machLimit || machRtrn === Yes){
-    
+var Yes = true;
+
+if(modelPoly <= machLimit || machRtn === Yes){
+    console.log("You can use the character model with little or no problems.");
+}else{
+    console.log("You'd better find a lower poly character model.")
 }
