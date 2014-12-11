d6f598b2e8332d8bf9f46db78ab35ddf9fa794ef
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-11 11:59:31 -0600

    completed commenting
    Completed commenting and changed minutes to hours in second prompt.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,17 +7,16 @@ consideration how many calories your favorite exercise burns per hour. Lastly, y
 
 var caloriesBrn = prompt("Enter the number of calories you need to burn by the end of your workout.")
     //This line of code prompts the user to enter the number of calories they wish to burn during their entire workout.
-    console.log(caloriesBrn); //This prints the entered variable to the console.
 
-var averageBrn = prompt("Enter the number of calories typically burned per minute doing your selected workout.")
+var averageBrn = prompt("Enter the number of calories typically burned per hour doing your selected workout.")
     //This line of code prompts the user to enter the average number of calories burned per hour with selected workout.
-    console.log(averageBrn); //This prints the entered variable to the console.
 
-var timeToWorkout = prompt("Enter your intended workout time in hours. Round your entry to the nearest hour.")
-    //This line of code prompts the user to enter their intended workout time rounded to the hour.
-    console.log(timeToWorkout); //This prints the entered variable to the console.
+var timeToWorkout = prompt("Enter your intended workout time in hours.")
+    //This line of code prompts the user to enter their intended workout time.
 
-var workOut;
+var workOut; //This line of code identifies the workout variable.
 
 workOut = (averageBrn * timeToWorkout >= caloriesBrn) ? "You can burn the planned number of calories in your selected workout time. Go for it!" : "You need to adjust how many calories you plan to burn or how long you should workout."
-console.log(workOut)
+    //This line of code uses multiplication with average burn per hour and workout time and determines if the user will
+    //burn equal to or greater than their desired number of calories based on their exercise and time limit.
+    console.log(workOut) //This line of code prints the resulting workout variable information to the console.
