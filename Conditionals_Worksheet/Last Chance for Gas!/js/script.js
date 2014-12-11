4daa7d5f1e8d380f50398eb60bdc1bf8cbae32e1
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-10 21:02:09 -0600

    consoles
    Added console.log code for get gas and don’t get gas

--- Conditionals_Worksheet/Last Chance for Gas!/js/script.js
+++ Conditionals_Worksheet/Last Chance for Gas!/js/script.js
@@ -3,6 +3,10 @@ Conditionals Worksheet Jonathan Daniel 12/10/2014.
 Last Chance for Gas
  */
 
+//A driver has to determine if they can make it across the desert with their current fuel.
+// They are about to past the last gas station for the next 200 miles and they need to determine
+// whether they should stop now for gas or not.
+
 var gasEfficiency = prompt ("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:");
 //This line of code is to be used to collect data related to the fuel efficiency of the vehicle.
 
@@ -12,7 +16,7 @@ var quageReading = prompt ("Now, enter the amount of gas you currently have as a
 var carCapacity = prompt ("Now, enter your car's gas tank capacity:");
 //This line of code ot to be used to collect the vehicle's fuel capacity.
 
-
+//If the vehicle has enough fuel  or is fuel efficient to make it on the current amount of fuel.
 if(){
     console.log("Yes, you can make it without stopping for gas!");
 }else{
