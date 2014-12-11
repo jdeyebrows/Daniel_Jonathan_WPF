f0a84e2d5f3681c706a6986bc5379bd40cc41ae8
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-10 19:23:14 -0600

    if else
    added empty if and else statement to last chance for gas.

--- Conditionals_Worksheet/Last Chance for Gas!/js/script.js
+++ Conditionals_Worksheet/Last Chance for Gas!/js/script.js
@@ -3,9 +3,21 @@ Conditionals Worksheet Jonathan Daniel 12/10/2014.
 Last Chance for Gas
  */
 
-var gasEfficiency = prompt ("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:")
+var gasEfficiency = prompt ("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:");
+//This line of code is to be used to collect data related to the fuel efficiency of the vehicle.
 
-var quageReading = prompt ("Now, enter the amount of gas you currently have as a percentage. What percent of your tank is full:)
+var quageReading = prompt ("Now, enter the amount of gas you currently have as a percentage. What percent of your tank is full:);
+//This line of code is to be used to collect the percentage of fuel the vehicle currently has.
 
-var carCapacity = prompt ("Now, enter your car's gas tank capacity:")
+var carCapacity = prompt ("Now, enter your car's gas tank capacity:");
+//This line of code ot to be used to collect the vehicle's fuel capacity.
+
+
+if(){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have X gallons of gas in your tank, better get gas now while you can!");
+}
+
+
 
