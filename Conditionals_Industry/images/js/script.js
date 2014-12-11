d1b561c2d6c09a1bfc01bddb990167f3361874ce
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-11 13:35:19 -0600

    Added Industry
    Added Conditionals Industry

---
+++ Conditionals_Industry/images/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Jonathan Daniel Conditionals Industry 12/11/14.
+ */
+
+/* Here, the user will evaluate the salary, and increased earning opportunity of a job they are interviewing for.
+They have been guaranteed the job and are in the process of making a decision. Their decision will be based on
+whether or not the job meets the user's salary. If the user's asking salary is not entirely met , the final decision
+will depend on whether the company offers desirable annual raises.
+ */
+
+var salaryReq = prompt("Please let us know your desired salary.")
+
+var salaryMin = prompt("Please let us know the minimum salary you will accept.")
+
+var salaryRs = prompt("enter 3rd prompt here")
+
+
