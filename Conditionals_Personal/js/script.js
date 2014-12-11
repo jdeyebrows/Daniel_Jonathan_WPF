9f2b45e1d84ab37102712cd8bac3953902ac2d71
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-11 11:49:25 -0600

    Added comments
    Added comments to Conditionals_Personal prompts.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,10 +1,21 @@
-//You want to get a great workout today, but you're not the best at time management.
-//You need to determine how many calories you want to burn.
+/* You want to get a great workout today, but you're not the best at time management.
+You need to determine how many calories you want to burn. You also need to take into
+consideration how many calories your favorite exercise burns per hour. Lastly, you
+ already know how much time you have available to workout. Input the needed information
+ to determine if your desired results are realistic based on the time limits of your workout
+ and the number or calories your activity will allow you to burn.*/
+
 var caloriesBrn = prompt("Enter the number of calories you need to burn by the end of your workout.")
+    //This line of code prompts the user to enter the number of calories they wish to burn during their entire workout.
+    console.log(caloriesBrn); //This prints the entered variable to the console.
 
-var averageBrn = prompt("Enter the number of calories burned per minute doing your selected workout.")
+var averageBrn = prompt("Enter the number of calories typically burned per minute doing your selected workout.")
+    //This line of code prompts the user to enter the average number of calories burned per hour with selected workout.
+    console.log(averageBrn); //This prints the entered variable to the console.
 
-var timeToWorkout = prompt("Enter your intended workout time in hours.")
+var timeToWorkout = prompt("Enter your intended workout time in hours. Round your entry to the nearest hour.")
+    //This line of code prompts the user to enter their intended workout time rounded to the hour.
+    console.log(timeToWorkout); //This prints the entered variable to the console.
 
 var workOut;
 
