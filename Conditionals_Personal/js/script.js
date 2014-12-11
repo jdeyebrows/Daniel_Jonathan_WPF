5e8707e687f1d19a9fef3483cd599244cf481369
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-11 10:15:17 -0600

    Added Variables
    Added variable to Conditionals_Personal.

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Jonathan Daniel Conditionals_Professionals 12/11/14.
+ */
+//The Film Festival.  We have to determine if your fim meets the required standards to be included
+//as part of our fall film festival.
+
+var filmFormat = prompt("We need to determine that your film is in our required viewing format. \nPease verify " +
+"your film's format by entering PAL or NTSC. Only these two formats are accepted for preview")
+
+var feePaid = prompt("Have you already Completed your online submissions form? Enter Y for yes or N for No. If the " +
+"submission deadline has already passed, your film cannot be accepted as the fee is due before the deadline.")
+
+var filmLength = prompt("The time limit for all film submissions is 30 minutes. Please enter the running time of " +
+"your film to determine if your film is eligible for entry. the fee is waived for films under 10 minutes and the online " +
+"submission for is not required.")
