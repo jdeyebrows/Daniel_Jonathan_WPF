ce6c68f331eced5900afbcff100dc280aede26d7
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-10 21:40:08 -0600

    attempting
    Attempting to find out why prompts won’t show.

--- Conditionals_Worksheet/Last Chance for Gas!/js/script.js
+++ Conditionals_Worksheet/Last Chance for Gas!/js/script.js
@@ -1,23 +1,10 @@
-/*
-Conditionals Worksheet Jonathan Daniel 12/10/2014.
-Last Chance for Gas
- */
+var gasEf = prompt("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:")
 
-//A driver has to determine if they can make it across the desert with their current fuel.
-// They are about to past the last gas station for the next 200 miles and they need to determine
-// whether they should stop now for gas or not.
+var gRead = prompt("Now, enter the amount of gas you currently have as a percentage. What percent of your tank is full:")
 
-var gasEfficiency = prompt ("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:");
-//This line of code is to be used to collect data related to the fuel efficiency of the vehicle.
+var carCap = prompt("Now, enter your car's gas tank capacity:")
 
-var quageReading = prompt ("Now, enter the amount of gas you currently have as a percentage. What percent of your tank is full:);
-//This line of code is to be used to collect the percentage of fuel the vehicle currently has.
-
-var carCapacity = prompt ("Now, enter your car's gas tank capacity:");
-//This line of code ot to be used to collect the vehicle's fuel capacity.
-
-//If the vehicle has enough fuel  or is fuel efficient to make it on the current amount of fuel.
-if(){gasEfficiency * carCapacity / quageReading > 200){
+if(){gasEf * carCap / gRead > 200){
     console.log("Yes, you can make it without stopping for gas!");
 }else{
     console.log("You only have X gallons of gas in your tank, better get gas now while you can!");
