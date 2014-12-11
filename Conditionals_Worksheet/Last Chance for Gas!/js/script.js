8a5d7ee975834753749ce5f8f5b9a0388866d739
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-10 19:04:39 -0600

    Added prompts
    Added prompts on last chance for gas

--- Conditionals_Worksheet/Last Chance for Gas!/js/script.js
+++ Conditionals_Worksheet/Last Chance for Gas!/js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by jonathandaniel on 12/9/14.
+/*
+Conditionals Worksheet Jonathan Daniel 12/10/2014.
+Last Chance for Gas
  */
+
+var gasEfficiency = prompt ("In order to determine whether or not you need to stop for fuel, \nenter your vehicle's gas efficiency in MPG:")
+
+var quageReading = prompt ("Now, enter the amount of gas you currently have as a percentage. What percent of your tank is full:)
+
+var carCapacity = prompt ("Now, enter your car's gas tank capacity:")
+
