843e4929ad14b961d07875f407e631038479ec8b
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-12 16:09:51 -0600

    Parameters and Arguments
    Parameters and Arguments

--- Basic Function Structure/Basic Function Structure/js/script.js
+++ Basic Function Structure/Basic Function Structure/js/script.js
@@ -1,14 +1,3 @@
-/* Basic Function Structure Jonathan Daniel 12/12/14 */
-//Functions - Function Execution
+//Functions - Parameters and Arguments
 
 
-var width = 5;
-
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
-    console.log(area);
-}
-
-calcArea();
