7713fc98e924112431f95109c6d58b9121757f17
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-12 10:50:16 -0600

    Created a for loop
    Created a for loop

--- GoToTrainingWeek3/js/week3.js
+++ GoToTrainingWeek3/js/week3.js
@@ -79,3 +79,35 @@ if(bowlOfFruit[3]==="pear"){
 
 //Report how many pears we have
 console.log("There are "+totalNumPears+" number of pears in the bowl");
+
+//We want the computer to do the "heavy lifting" for us
+
+
+//Create a new tracking variable for number of pears
+var pearNumber= 0;
+
+
+//Create a loop for repetitive code
+
+//For Loop - is great for when you know how many times it should run
+// for(initialize a counting variable; codition to test; incremental change)
+// { code to run each loop around }
+
+for(var i=0;i<8; i++ ){
+    console.log("Inside of the loop"+i);
+    console.log(bowlOfFruit[i]);
+
+    //Test each fruit if it is a pear
+    if(bowlOfFruit[i]==="pear") {
+        //Add to our pear total
+        pearNumber++;
+        console.log("This fruit is a pear!");
+    }else{
+        console.log("This fruit is not a pear!");
+
+    }
+
+
+}
+
+console.log("Total number of pears in the bowl is "+pearNumber);
