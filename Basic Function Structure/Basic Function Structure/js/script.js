ee70667a9e6cf90a20c78b91d9391c32c8512ed0
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-12 16:24:53 -0600

    Console Print Sparky's Age
    Console Print Sparky's Age

--- Basic Function Structure/Basic Function Structure/js/script.js
+++ Basic Function Structure/Basic Function Structure/js/script.js
@@ -1,8 +1,7 @@
 //Functions - Parameters and Arguments
 
-calcArea(30, 20);
-
-function calcArea(w, h){ //w=30, h=20
-    var area = w * h;
-    console.log(area);
+function dogYears(){
+    var age =4;
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
 }
