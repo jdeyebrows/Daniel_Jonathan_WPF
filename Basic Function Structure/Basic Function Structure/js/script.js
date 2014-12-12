67f0c9f86cc5c8353db98e6f3ebb3597ba49eb30
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-12 16:37:25 -0600

    Returning Values
    Returning Values

--- Basic Function Structure/Basic Function Structure/js/script.js
+++ Basic Function Structure/Basic Function Structure/js/script.js
@@ -1,9 +1,8 @@
-//Functions - Parameters and Arguments
+//Functions - Returning Values
 
-function dogYears(age){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old.")
+calcArea(30, 20);
+
+function calcArea(w, h){ //w=30, h=20
+    var area = w * h;
+    console.log(area);
 }
-var age1 = 4
-dogYears(age1);
-dogYears(5);
