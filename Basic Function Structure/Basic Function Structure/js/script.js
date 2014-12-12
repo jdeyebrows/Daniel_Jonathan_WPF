ef511fcee8d03a41ffdea5c044007ea00bc29e61
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-12 15:35:58 -0600

    Added Basic Function Structure
    Added Basic Function Structure

---
+++ Basic Function Structure/Basic Function Structure/js/script.js
@@ -0,0 +1,16 @@
+/* Basic Function Structure Jonathan Daniel 12/12/14 */
+//Functions - Function Execution
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
