d27e74fa559e47bb63d21da2a04731b89ab2d609
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-15 19:25:11 -0600

    Added Do While Loop
    Added Do While Loop

--- Basic Function Structure/Screencast_While_Loop/js/script.js
+++ Basic Function Structure/Screencast_While_Loop/js/script.js
@@ -1,8 +1,20 @@
 /* Screencast Loops */
 
-var b = 80; //Sets up the index
+console("-----Loops-----")
+
+var b = 10; //Sets up the index
 
 while(b > 0){ //Checks the condition
     console.log(b + "This is cool!");
     b--; //Increments or decrements the index
 }
+
+console("-----Do While Loops")
+
+var c = 10;
+
+do{
+    console.log(c + "This is cool!");
+    c--;
+}
+while(c > 0);
