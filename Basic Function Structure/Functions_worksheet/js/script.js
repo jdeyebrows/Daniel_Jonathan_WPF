c8b150fc71c973bf827c4a69dd58b1d12c8c8a62
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-15 23:17:26 -0600

    commented on function
    commented on function

--- Basic Function Structure/Functions_worksheet/js/script.js
+++ Basic Function Structure/Functions_worksheet/js/script.js
@@ -8,13 +8,13 @@ var radius = 12; //holds the value of the radius of the circle
 
 var total = calcCircumference(radius, 2, 3.14); //variable assigned to the function call
 
-function calcCircumference(a, b, c){
+function calcCircumference(a, b, c){ //indicates what values will be used
 
     var circumference = a * b * c;
 
     return circumference;
 }
-console.log(total);
+console.log(total); //prints the information collected by the variable assigned to the function call
 
 
 
