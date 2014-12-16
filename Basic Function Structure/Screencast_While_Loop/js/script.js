dad400562ecff6f163b11790a2b2e09e6160a9da
jdeyebrows <eyebrows_amazing@yahoo.com>
2014-12-15 19:16:58 -0600

    Setup While Loop
    Setup While Loop

---
+++ Basic Function Structure/Screencast_While_Loop/js/script.js
@@ -0,0 +1,8 @@
+/* Screencast Loops */
+
+var b = 80; //Sets up the index
+
+while(b > 0){ //Checks the condition
+    console.log(b + "This is cool!");
+    b--; //Increments or decrements the index
+}
